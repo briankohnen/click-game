ae6555312b92d7273a0c01fb5c3ece68f5ec3f65
BrianKohnen <briankohnen@yahoo.com>
2019-10-26 13:27:51 -0500

    Updates

---
+++ precache-manifest.21ab28e192021bbdddb5ed367113f3cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9772bd25de343ef4d4596289e73e70",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "d71ec21f6889a645b9f6",
+    "url": "/click-game/static/css/main.d5230995.chunk.css"
+  },
+  {
+    "revision": "3f866b05f6539c6a4c75",
+    "url": "/click-game/static/js/2.c3093c4b.chunk.js"
+  },
+  {
+    "revision": "d71ec21f6889a645b9f6",
+    "url": "/click-game/static/js/main.c51b1218.chunk.js"
+  },
+  {
+    "revision": "d5796a19539a529da0f8",
+    "url": "/click-game/static/js/runtime-main.ad9a6dd4.js"
+  }
+]);
